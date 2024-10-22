3a50d148abbf5aec6fb191e3e79b1b3768add782
RuPDuD143 <81021778+RuPDuD143@users.noreply.github.com>
2024-10-23 01:34:35 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
-<?php
-echo "hello";
-?>
+
+<html>
+  <head>
+    <title>Testing Website</title>
+  </head>
+  <body>
+    <h1>What is up with you</h1>
+    <?php
+    echo 'hello';
+    ?>
+  </body>
+</html>
